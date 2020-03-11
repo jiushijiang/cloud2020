d85b753ddbb7201307be84a172566ffe404805af
huangxiaoyang <123456@qq.com>
2020-03-11 19:42:13 +0800

    添加支付新项目

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/PaymentMain80.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain80.class,args);
+    }
+}
